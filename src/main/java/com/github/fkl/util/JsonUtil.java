e16eeb3c14948022d58d7201af4b8cdd2f5b602d
kangle.fan <kangle.fan@qunar.com>
2017-05-25 11:53:08 +0800

    add

--- src/main/java/com/github/fkl/util/JsonUtil.java
+++ src/main/java/com/github/fkl/util/JsonUtil.java
@@ -2,19 +2,38 @@ package com.github.fkl.util;
 
 import com.github.fkl.bean.Test;
 import com.github.fkl.util.json.JsonBase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.InputStream;
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Proxy;
+import java.util.Properties;
 
 /**
  * Created by kanglefan on 17-5-24.
  */
 public class JsonUtil {
 
+    private static Logger logger = LoggerFactory.getLogger(JsonUtil.class);
+
+    private static final String CONF_PATH = "/system.properties";
+    private static final String CONST_CLASS = "json.class.name";
     private static JsonBase base;
 
     static {
-        new JsonUtil("com.github.fkl.util.json.JacksonUtil");
+        try {
+            // 读取配置文件获取类
+            Properties properties = new Properties();
+            InputStream in = JsonUtil.class.getResourceAsStream(CONF_PATH);
+            if (in == null) {
+                throw new RuntimeException("input stream get fail");
+            }
+            properties.load(in);
+            new JsonUtil(properties.getProperty(CONST_CLASS));
+        } catch (Exception e) {
+            logger.error("JsonUtil static exec error", e);
+        }
     }
 
     private JsonUtil(String className) {
@@ -24,7 +43,9 @@ public class JsonUtil {
                 base = (JsonBase) Proxy.newProxyInstance(JsonBase.class.getClassLoader(),
                         new Class[]{JsonBase.class}, (InvocationHandler) clazz.newInstance());
             }
-        } catch (Exception e) {}
+        } catch (Exception e) {
+            logger.error("JsonUtil init error", e);
+        }
     }
 
     public static String encode(Object obj) {
