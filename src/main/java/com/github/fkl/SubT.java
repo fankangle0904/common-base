cf97f52222316e52bd9c83b6acc2e3cf2e3b7865
kangle.fan <kangle.fan@qunar.com>
2017-07-24 17:17:55 +0800

    fix

--- src/main/java/com/github/fkl/SubT.java
+++ src/main/java/com/github/fkl/SubT.java
@@ -1,5 +1,10 @@
 package com.github.fkl;
 
+import java.net.URL;
+import java.util.Enumeration;
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Created by kanglefan on 17-6-7.
  */
@@ -17,7 +22,24 @@ public class SubT extends T {
 
     public static Integer b = a;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         System.out.println(b);
+
+        String path = SubT.class.getName().replace('.', '/') + ".class";
+        Enumeration<URL> urls = T.class.getClassLoader().getResources(path);
+        Set<String> files = new HashSet<String>();
+        while (urls.hasMoreElements()) {
+            URL url = urls.nextElement();
+            if (url != null) {
+                String file = url.getFile();
+                System.out.println(file);
+                if (file != null && file.length() > 0) {
+                    files.add(file);
+                }
+            }
+        }
+        if (files.size() > 1) {
+            System.out.println("Duplicate class " + path + " in " + files.size() + " jar " + files);
+        }
     }
 }
